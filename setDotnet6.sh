6872352103963e6ec37253cdc2950561f531c3c9
James <james.lewes@fisglobal.com>
2021-12-26 17:36:56 +0000

    rename

---
+++ setDotnet6.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+ # envoke using . ./setDotnet6.sh
+ 
+ export DOTNET_ROOT=~/dotnet-sdk-6.0.101-win-x86/
+ export PATH=~/dotnet-sdk-6.0.101-win-x86:$PATH 
+ export DOTNET_MULTILEVEL_LOOKUP=0
